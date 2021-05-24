80bbf370c24ac7e704b88f34d637a0d10c4d3d53
Snehil Suresh Wakchaure <snehil.w@gmail.com>
2021-05-24 12:15:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,202 +1,161 @@
-const https = require('https');
-var AWS = require("aws-sdk");
+const https   = require('https');
+const AWS     = require("aws-sdk");
 const Twitter = require("twitter");
 
-// [TODO] UPDATE THESE DETAILS FOR THE DESIRED LOCATION OR DEPLOYMENT ENVIRONMENT
-const smsTopicArn = '<SNS_TOPIC_ARN>'; // Deprecated
-const location = "PUNE";
-const maxAgeLimit = 200;
-
 // Set region
-AWS.config.update({region: 'ap-south-1'});
+AWS.config.update({ region: 'ap-south-1' });
 
-function formatDate(date) {
-    var d = new Date(date),
+const formatDate = date => {
+    var d     = new Date(date),
         month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
+        day   = '' + d.getDate(),
+        year  = d.getFullYear();
 
-    if (month.length < 2)
-        month = '0' + month;
-    if (day.length < 2)
-        day = '0' + day;
+    if (month.length < 2) month = '0' + month;
+    if (day.length   < 2) day   = '0' + day;
 
     return [day, month, year].join('-');
 }
 
-const postTweet = msg => { 
-  return new Promise((resolve, reject) => {
-    console.log(msg);
-
-    var client = new Twitter({
-      consumer_key: process.env.TWITTER_CONSUMER_KEY,
-      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-      access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-      access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+const postTweet = (twitterClient, msg) => { 
+    return new Promise((resolve, reject) => {
+        console.info(`Posting tweet - ${msg}`);
+        return twitterClient.post('statuses/update', {status: msg}).catch(console.error);
+        // return Promise.resolve();
     });
+};
 
-    return client.post('statuses/update', {status: msg}).catch(console.error);
-  });
+const validateInput = (event, context, prop) => {
+    if (!event.hasOwnProperty(prop)) {
+        context.fail(new Error(`${prop} not specified in input`));
+    }
 };
 
 const today = formatDate(new Date());
 
 exports.handler = async (event, context) => {
-    let dataString = '';
-    
-    console.log("Input:" + JSON.stringify(event, null, 2));
+    let   dataString     = '';
+    const maxAgeLimit    = 200;
+    const maxTweetLength = 280;
 
-    if (!event.hasOwnProperty("districtId")) {
-        context.fail(new Error("districtId not specified in input."));
-        return null;
-    }
-    
-    const districtId = event.districtId || 0;
-    let url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id="+ districtId +"&date="+ today;
-    
-    const formatMessage = msg => {
-      return msg;
-    };
+    console.info(`Input: ${JSON.stringify(event, null, 2)}`);
 
+    // Input validation
+    validateInput(event, context, 'districtId');
+    validateInput(event, context, 'consumerKey');
+    validateInput(event, context, 'consumerSecret');
+    validateInput(event, context, 'accessTokenKey');
+    validateInput(event, context, 'accessTokenSecret');
+
+    const twitterClient = new Twitter({
+        consumer_key        : event.consumerKey,
+        consumer_secret     : event.consumerSecret,
+        access_token_key    : event.accessTokenKey,
+        access_token_secret : event.accessTokenSecret
+    });
+
+    const url = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${event.districtId || 0}&date=${today}`;
+    
     const response = await new Promise((resolve, reject) => {
-        const req = https.get(url, function(res) {
+        const req = https.get(url, res => {
          
-          res.on('data', chunk => {
-            dataString += chunk;
-          });
+        res.on('data', chunk => { dataString += chunk });
          
-          res.on('end', () => {
-            const response = JSON.parse(dataString);
+        res.on('end', () => {
+            const response         = JSON.parse(dataString);
             const availableCenters = [];
    
-            if (!response.hasOwnProperty('centers') || typeof(response.centers) === 'undefined' || (response.centers === null)) {
-              return;
-            }
+            if (!response.hasOwnProperty('centers') || response.centers == null) return;
    
-            const centers       = response.centers;
             const tweetPromises = [];
             const tweetMessages = new Set();
-            // console.log(JSON.stringify(centers, null, 2));
 
-            centers.forEach(center => {
-              if (center.hasOwnProperty('sessions')) {
-                  const sessions = center.sessions;
-                     
-                  if (Array.isArray(sessions) && sessions.length > 0) {
+            response.centers.forEach(center => {
+                if (center.hasOwnProperty('sessions')) {
+                    const sessions = center.sessions;
+                       
+                    if (Array.isArray(sessions) && sessions.length > 0) {  
+                        center.address = `${center.address}, ${center.block_name}, ${center.district_name}, ${center.state_name}, ${center.pincode}`;
 
-                      // remove unwanted properties
-                      center.address = `${center.address}, ${center.block_name}, ${center.district_name}, ${center.state_name}, ${center.pincode}`;
-                      delete center.center_id;
-                      delete center.state_name;
-                      delete center.district_name;
-                      delete center.block_name;
-                      delete center.pincode;
-                      delete center.lat;
-                      delete center.long;
-                      delete center.from;
-                      delete center.to;
-                        
-                    sessions.forEach(session => {
-                      if (session.hasOwnProperty('available_capacity') &&
-                        session.available_capacity !== null           &&
-                        session.available_capacity !== 'undefined'    &&
-                        session.available_capacity > 0                && 
-                        session.hasOwnProperty('min_age_limit')       &&
-                        session.min_age_limit !== null                &&
-                        session.min_age_limit !== 'undefined'         &&
-                        session.min_age_limit < maxAgeLimit) {
-                          
-                        // remove unwanted properties
-                        delete session.session_id;
-                         
-                        availableCenters.push(formatMessage(center));
-                      }
-                    });
-                  }
-               
-              } // else do nothing
+                        sessions.forEach(session => {
+                            if (session.hasOwnProperty('available_capacity') &&
+                                session.available_capacity != null           &&
+                                session.available_capacity > 0) {
+                                 
+                                availableCenters.push(center);
+                            }
+                        });
+                    }
+                } // else do nothing
             });
            
-            console.log(`Available Centers: ${availableCenters}`);
+            console.info(`Available Centers: ${availableCenters}`);
 
             if (Array.isArray(availableCenters) && availableCenters.length > 0) {
-               var eventText = JSON.stringify(availableCenters, null, 2);
-               var subject = `[${location}] Cowin vaccine availability update `;
-             
-               var params = {
-                   Message: eventText,
-                   Subject: subject,
-                   TopicArn: smsTopicArn
-               };
+                availableCenters.forEach(center => {
+                    const centerName    = center.name    || '';
+                    let   centerAddress = center.address || '';
+                    let   vaccineNames  = new Set();
+                    let   totalQty      = 0;
+                    let   dose1Qty      = 0;
+                    let   dose2Qty      = 0;
+                    let   ages          = new Set(); 
+                    let   dates         = new Set();
 
-              // POST Vaccine availability updates to Twitter
-              console.log("Posting tweet - " + JSON.stringify(availableCenters, null, 2));
+                    center.sessions.forEach(session => {
+                        if (session.vaccine       || '' != '') vaccineNames.add(session.vaccine);
+                        if (session.min_age_limit || 0  != 0)  ages.add(session.min_age_limit + '+');
 
-              availableCenters.forEach(center => {
-                const centerName    = center.name    || '';
-                let   centerAddress = center.address || '';
-                let   vaccineNames  = new Set();
-                let   totalQty      = 0;
-                let   dose1Qty      = 0;
-                let   dose2Qty      = 0;
-                let   ages          = new Set(); 
-                let   dates         = new Set();
+                        totalQty += session.available_capacity;
+                        dose1Qty += session.available_capacity_dose1;
+                        dose2Qty += session.available_capacity_dose2;
+                        
+                        const dateArr       = (session.date).split('-');
+                        const formattedDate = `${dateArr[0]}/${dateArr[1]}`;
 
-                center.sessions.forEach(session => {
-                  if (session.vaccine || '' != '') vaccineNames.add(session.vaccine);
-                  if (session.min_age_limit || 0 != 0) ages.add(session.min_age_limit + '+');
-                  totalQty += session.available_capacity;
-                  dose1Qty += session.available_capacity_dose1;
-                  dose2Qty += session.available_capacity_dose2;
-                  
-                  const dateArr = (session.date).split('-');
-                  const formattedDate = `${dateArr[0]}/${dateArr[1]}`;
-                  dates.add(formattedDate);
+                        dates.add(formattedDate);
+                    });
+
+                    vaccineNames      = Array.from(vaccineNames).join(',');
+                    ages              = Array.from(ages).join(',');
+                    dates             = Array.from(dates).join(', ');
+                    const todaySplits = today.split('-');
+
+                    // Build Tweet message
+                    let tweetMsg = `[${todaySplits[0]}/${todaySplits[1]}] ${vaccineNames} (Dose 1 Qty-${dose1Qty}, Dose 2 Qty-${dose2Qty}, Ages-${ages}, Dates-${dates}) available at - ${centerName}, ${centerAddress}`;
+                    tweetMsg     += `, Details-https://www.cowin.gov.in`;
+
+                    if (dose1Qty > 0 || dose2Qty > 0) {
+                        if (tweetMsg.length > maxTweetLength) {
+                            const msg1 = tweetMsg.substring(0, maxTweetLength);
+                            const msg2 = `[${todaySplits[0]}/${todaySplits[1]}] ${tweetMsg.substring(maxTweetLength, tweetMsg.length)}`;    
+
+                            tweetMessages.add(msg1);
+                            tweetMessages.add(msg2);
+                        } else {
+                            tweetMessages.add(tweetMsg);
+                        }
+                    }
                 });
-
-                vaccineNames = Array.from(vaccineNames).join(',');
-                ages         = Array.from(ages).join(',');
-                dates        = Array.from(dates).join(', ');
-                
-                const todaySplits = today.split('-');
-
-                // Build Tweet message
-                let tweetMsg = `[${todaySplits[0]}/${todaySplits[1]}] ${vaccineNames} (Dose 1 Qty-${dose1Qty}, Dose 2 Qty-${dose2Qty}, Ages-${ages}, Dates-${dates}) available at - ${centerName}, ${centerAddress}`;
-                tweetMsg += `, Details-https://www.cowin.gov.in`;
-
-                if (dose1Qty > 0 || dose2Qty > 0) {
-                  if (tweetMsg.length > 280) {
-                    const msg1 = tweetMsg.substring(0, 280);
-                    const msg2 = `[${todaySplits[0]}/${todaySplits[1]}] ${tweetMsg.substring(280, tweetMsg.length)}`;
-
-                    tweetMessages.add(msg1);
-                    tweetMessages.add(msg2);
-                  } else {
-                    tweetMessages.add(tweetMsg);
-                  }
-                }
-              });
             }
 
-            Array.from(tweetMessages).forEach(msg => {
-              tweetPromises.push(postTweet(msg));
-            });
+            Array.from(tweetMessages).forEach(msg => tweetPromises.push(postTweet(twitterClient, msg)));
 
             // Post tweets
             if (tweetPromises.length > 0) {
-              Promise.all(tweetPromises)
-                .then(console.log)
-                .catch(console.error);
+                Promise.all(tweetPromises)
+                    .then(console.log)
+                    .catch(console.error);
             }
             // else do nothing
           });
         });
        
-        req.on('error', (e) => {
-          reject({
-              statusCode: 500,
-              body: 'Something went wrong!'
-          });
+        req.on('error', err => {
+            reject({
+                statusCode : 500,
+                body       : err.message || 'Something went wrong'
+            });
         });
     });
    
